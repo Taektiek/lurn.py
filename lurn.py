a3abacd1bb71c6ec3fcfcaf38687ae01fccfac53
TaekeRoukema <62061406+TaekeRoukema@users.noreply.github.com>
2020-08-22 18:50:29 +0200

    Added Word() class

--- lurn.py
+++ lurn.py
@@ -1,5 +1,11 @@
 import json
 
+class Word():
+    
+    def __init__(self, original, translation):
+        self.original = original
+        self.translation = translation
+
 def load_file(file_loc):
     word_input = open(file_loc)
     return word_input.read()
