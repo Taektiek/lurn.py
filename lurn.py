db4353fec1ca15f9d2ccdaa50ec1615a52a4e0ad
TaekeRoukema <62061406+TaekeRoukema@users.noreply.github.com>
2020-08-22 19:31:26 +0200

    Added state system

--- lurn.py
+++ lurn.py
@@ -6,6 +6,9 @@ class Word():
         self.original = original
         self.translation = translation
 
+        self.state = 'Unseen'
+        self.in_sprint = False
+
 def load_file(file_loc):
     word_input = open(file_loc)
     return word_input.read()
@@ -18,9 +21,31 @@ def create_words(words_dict):
 
     return out
 
+def generate_state_dict(words):
+    states = {}
+    for i in words:
+        if i.state in states:
+            states[i.state].append(i)
+        else:
+            states[i.state] = [i]
+
+    return states
+
+def generate_sprint_list(words):
+    sprint = []
+    for i in words:
+        if i.in_sprint:
+            sprint.append(i)
+
+    return sprint
+
 def main():
-    words = json.loads(load_file('./wordsets/spanish-dutch_test.json'))
-    print(create_words(words))
+    words_dict = json.loads(load_file('./wordsets/spanish-dutch_test.json'))
+    words = create_words(words_dict)
+    words[1].state = 'Seen'
+    words[3].in_sprint = True
+    print(generate_state_dict(words))
+    print(generate_sprint_list(words))
 
 if __name__ == "__main__":
     main()
