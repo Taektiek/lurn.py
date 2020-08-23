ccdcb096bf3ee06b1e8e534f2c1064d0501a71e7
TaekeRoukema <62061406+TaekeRoukema@users.noreply.github.com>
2020-08-23 10:43:52 +0200

    Moved functions to class

--- lurn.py
+++ lurn.py
@@ -9,43 +9,54 @@ class Word():
         self.state = 'Unseen'
         self.in_sprint = False
 
+class Questions():
+    @staticmethod
+    def create_words(words_dict):
+        out = []
+
+        for i in words_dict["words"]:
+            out.append(Word(i["original"], i["translation"]))
+
+        return out
+
+    @staticmethod
+    def generate_state_dict(words):
+        states = {}
+        for i in words:
+            if i.state in states:
+                states[i.state].append(i)
+            else:
+                states[i.state] = [i]
+
+        return states
+
+    @staticmethod
+    def generate_sprint_list(words):
+        sprint = []
+        for i in words:
+            if i.in_sprint:
+                sprint.append(i)
+
+        return sprint
+
+    @staticmethod
+    def update_sprint(words, sprint):
+        pass
+
+
 def load_file(file_loc):
     word_input = open(file_loc)
     return word_input.read()
 
-def create_words(words_dict):
-    out = []
 
-    for i in words_dict["words"]:
-        out.append(Word(i["original"], i["translation"]))
-
-    return out
-
-def generate_state_dict(words):
-    states = {}
-    for i in words:
-        if i.state in states:
-            states[i.state].append(i)
-        else:
-            states[i.state] = [i]
-
-    return states
-
-def generate_sprint_list(words):
-    sprint = []
-    for i in words:
-        if i.in_sprint:
-            sprint.append(i)
-
-    return sprint
 
 def main():
     words_dict = json.loads(load_file('./wordsets/spanish-dutch_test.json'))
-    words = create_words(words_dict)
+    words = Questions.create_words(words_dict)
     words[1].state = 'Seen'
     words[3].in_sprint = True
-    print(generate_state_dict(words))
-    print(generate_sprint_list(words))
+    print(Questions.generate_state_dict(words))
+    print(Questions.generate_sprint_list(words))
 
 if __name__ == "__main__":
     main()
