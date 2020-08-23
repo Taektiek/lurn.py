c2b285dd8e3f3fdff1a2ca1854d1e192bd3909f0
TaekeRoukema <62061406+TaekeRoukema@users.noreply.github.com>
2020-08-23 13:32:57 +0200

    Added docstrings

--- lurn.py
+++ lurn.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import json
 import random
 
@@ -23,6 +26,15 @@ class Vocabulary():
 
     @staticmethod
     def create_words(words_dict):
+        """Creates word objects from dictionary
+
+        Args:
+            words_dict (dictionary): A dictionary with a list words with seperate dictionaries 
+            with an original and translation key
+
+        Returns:
+            list: A list of Word objects
+        """
         out = []
 
         for i in words_dict["words"]:
@@ -31,6 +43,11 @@ class Vocabulary():
         return out
 
     def generate_state_dict(self):
+        """Generates a dictionary with lists of objects and each state as a key
+
+        Returns:
+            dictionary: a dictionary with lists per state
+        """
         states = {}
         for i in self.words:
             if i.state in states:
@@ -41,16 +58,28 @@ class Vocabulary():
         return states
 
     def update_sprint(self):
+        """Updates the sprint to be have sprint_length as its length. Randomly picks Unseen word objects.
+        """
         self.sprint += random.sample(self.generate_state_dict()["Unseen"], self.sprint_length - len(self.sprint))
         for i in self.sprint:
             i.in_sprint = True
 
     def generate_round(self):
+        """Generates the round from the current sprint
+        """
         self.active = random.sample(self.sprint, self.sprint_length)
             
 
 
 def load_file(file_loc):
+    """Loads a file and returns the string
+
+    Args:
+        file_loc (string): Location of the file
+
+    Returns:
+        string: contents of the file
+    """
     word_input = open(file_loc)
     return word_input.read()
 
